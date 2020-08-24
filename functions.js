f576b322ba98b3d0ad393b9385b062f04a8c97f2
mkrumins <marcis@burti.lv>
2020-08-24 23:56:35 +0300

    upload

---
+++ functions.js
@@ -0,0 +1,14 @@
+// Funkcija, kas ar jquery noslēpj <div> pēc klases
+function hide_div () { // vecais funkcijas definējums
+        $(".hide").click(function() { // atrod klasi "hide", izsauc funkciju uz klikšķa
+          $(this).hide(); // Selects the current HTML element + izsauc hide() funkciju
+        })
+}
+
+test1 = () => { //jaunais stils (arrow function expression)
+  //alert("This is alert 2");
+}
+ 
+
+
+
---
+++ sample.js
@@ -0,0 +1,20 @@
+// Mainīgo tipi
+
+// var  - globāls 
+// let -  funbkcijas ietvaros
+// const - konstantne, nevar mainīt 
+// semikoli obligāti ?
+
+var jaunsobjektsa  = {} // - objkekts
+var jaunsobjektsa2  = {a1:123} // objekts ar mainīgo, kam piesaistīta vērtība (atslēga:vērtība vai mainīgais:vērtība)
+var jaunsobjektsa2  = {a1:123, a2:"Hello"} // ar divām vērtībām
+jaunsobjektsa2.a1 // dabūs ārā tikai vienu no vērtībām  - a1
+
+var vilciens = [] //masīvs (nav obligati jābut viens tips, bet labāk likt vienu)
+var test = [2,3,"koks"] // ar dažādiem tipiem
+var vilciens = [1,4,10];
+vilciens[0] // pēc indexa parāda vērtību
+var years = new Array(1990, 2020); // jaunais masīva stils
+
+
+

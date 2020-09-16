efba8a82156de4f0f05edf908626deb806db06d7
mkrumins <marcis@burti.lv>
2020-09-16 12:24:30 +0300

    7 uzdevums

--- 14.09.2020/functions.js
+++ 14.09.2020/functions.js
@@ -1,3 +1,25 @@
-functionName = () => {
-  alert("strādā");
+fibonacciGen = () => {
+  let fibo = 0;
+  let nextFibo = 1;
+  let temp;
+  const element = 20;
+  const fiboDiv = $("#fibonacci").empty();
+
+  for (let i = 0; i < element; i++) {
+    temp = fibo;
+    fibo += nextFibo;
+    nextFibo = temp;
+
+    if ($("#Check")[0].checked) {
+      if (i == (element - 1)) {
+        fiboDiv.append(fibo);
+      } else {
+       fiboDiv.append(fibo + ", ");
+      }
+    }
+    else if (i == (element - 1)) {
+      fiboDiv.append(fibo);
+    }
+  }
 }
+
